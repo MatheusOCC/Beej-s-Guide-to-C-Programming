612f9e0b6b5f34103eee26a2d7408077302ecbae
MatheusCequeira <escrevaparamatheus@gmail.com>
2020-10-29 12:17:52 -0300

    criado repositório

---
+++ SumOfDesiredNumbers.c
@@ -0,0 +1,12 @@
+/*
+    Assignment:
+
+Implement a program that will calculate the sum of all numbers between 1 and the number the user enters.
+The program shall output the result.
+
+() Read the number from the keyboard using scanf().
+() Compute the sum of the numbers between one and the entered number using a for-loop and the addition operator.
+() Print the result using printf().
+
+*/
+
